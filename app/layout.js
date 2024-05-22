59868220f6c7924512da79bed52792047dd35da4
JeniaKhomenko <jeniaaajeniaaa@gmail.com>
2024-05-22 13:30:32 +0300

    second

--- app/layout.js
+++ app/layout.js
@@ -5,8 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "DecVolt",
-  description: "DecVolt is on a mission to redefine the future of energy",
+  title: "G&M Development",
+  description: "G&M Development",
 };
 
 export default function RootLayout({ children }) {
